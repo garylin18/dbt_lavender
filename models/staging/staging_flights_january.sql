3454954d53354042f8bff70af082a59b8652d3c7
Gary <garylin1118@GarydeMacBook-Air.local>
2025-03-12 11:51:14 +0100

    added mart file

--- models/staging/staging_flights_january.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-WITH flights_january AS (
-    SELECT * 
-    FROM {{source('flights_data', 'flights')}}
-    WHERE DATE_PART('month', flight_date) = 1 
-)
-SELECT * FROM flights_january
