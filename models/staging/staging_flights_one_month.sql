d47d92cfa84420bb2d583592fbb5b3400cf2454e
Gary <garylin1118@GarydeMacBook-Air.local>
2025-03-12 11:22:52 +0100

    modifed prep_flights

--- models/staging/staging_flights_one_month.sql
+++ models/staging/staging_flights_one_month.sql
@@ -2,7 +2,7 @@
     
     WITH flights_one_month AS (
         SELECT * 
-        FROM {{source('staging_flights', 'flights')}}
+        FROM {{source('staging_flights_january', 'flights')}}
         WHERE DATE_PART('month', flight_date) = 1 
     )
     SELECT * FROM flights_one_month
